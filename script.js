df5dd247f54de27752038f178ac840de5a225b69
mustafao2gur <mustafaseviss@gmail.com>
2023-01-24 02:59:23 +0300

    son

--- script.js
+++ script.js
@@ -16,41 +16,29 @@ select.addEventListener("click", (e) => {
       e.target.nextElementSibling.innerText--;
 
       cal();
-    }}
- else if (e.target.getAttribute("class") == "remove"){
-   if (confirm(` will be removed!`)
-    ) {
+    }
+  } else if (e.target.getAttribute("class") == "remove") {
+    if (confirm(` will be removed!`)) {
       e.target.closest(".product").remove();
     }
-cal()
-  } 
+    cal();
   }
-  
- )
+});
 
+const calculate = (target) => {
+  const x = target.closest(".info");
+  console.log(x);
+  let y = x.querySelector("div.price span").innerText;
+  console.log(y);
+  let z = x.querySelector("div.quantityBar  span.quantityNum").innerText;
 
-   
+  x.querySelector("p.productTotal span").innerText = z * y;
 
-const calculate=(target)=>{
-
- const x = target.closest(".info")
- console.log(x);
- let y =x.querySelector("div.price span").innerText
- console.log(y);
-let z = x.querySelector("div.quantityBar  span.quantityNum").innerText;
-
-x.querySelector("p.productTotal span").innerText=(z*y);
-
-
-//  let z =
-//    (e.target.parentElement.nextElementSibling.nextElementSibling.querySelector(
-//      ".productTotalNum"
-//    ).innerText = x * y);
-
-
-}
-
-
+  //  let z =
+  //    (e.target.parentElement.nextElementSibling.nextElementSibling.querySelector(
+  //      ".productTotalNum"
+  //    ).innerText = x * y);
+};
 
 function cal() {
   const productTotals = document.querySelectorAll(".productTotalNum");
@@ -69,7 +57,7 @@ function cal() {
   let tax1 = (tax.innerText = +(subTotal * 0.18).toFixed(2));
 
   total.innerText = +tax1;
-  if(total.innerText==0){
-vergi.innerText=0
+  if (total.innerText == 0) {
+    vergi.innerText = 0;
   }
 }
